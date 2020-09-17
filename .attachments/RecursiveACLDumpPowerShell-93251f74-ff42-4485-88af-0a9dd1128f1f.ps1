3b1a9dec67e55b1f855a3661fbf954d01fa551c6
Derek Yang <deya@microsoft.com>
2020-09-17 16:53:55 +0000

    Saving wiki attachment 'RecursiveACLDumpPowerShell-93251f74-ff42-4485-88af-0a9dd1128f1f.ps1'.

---
+++ .attachments/RecursiveACLDumpPowerShell-93251f74-ff42-4485-88af-0a9dd1128f1f.ps1
@@ -0,0 +1,30 @@
+﻿[cmdletbinding()]
+Param()
+
+Connect-AzAccount
+
+function recurseDataLakeStoreChildItem  ([hashtable] $Params, [string] $PathName) {
+    $ChildItems = Get-AzDataLakeStoreChildItem @Params;
+    $Path = $Params["Path"];
+    $Account = $Params["Account"];
+    Write-Verbose -Message "Removed ACLs from $Path";
+    Remove-AzDataLakeStoreItemAcl -AccountName $Account -Path $Path -Force;
+    foreach ($ChildItem in $ChildItems) {
+        switch ($ChildItem.Type) {
+            "FILE" {
+                $filepath = $Path+"/"+$ChildItem.Name;
+                Write-Verbose -Message "Removed ACLs from $filepath";
+                Remove-AzDataLakeStoreItemAcl -AccountName $Account -Path $filepath -Force;
+            }
+            "DIRECTORY" {
+                $Params.Remove("Path");
+                $Params.Add("Path", $Path + "/" + $ChildItem.Name);
+                $PathNameNew= $PathName+$ChildItem.Name+"\";
+                recurseDataLakeStoreChildItem -AllFiles $AllFiles -Params $Params -PathName $PathNameNew;
+            }
+        }
+    }
+}
+
+$PathName = "";
+$a = recurseDataLakeStoreChildItem -Params @{ 'Path' = '/testadlstool'; 'Account' = 'dyadlsg1'}
